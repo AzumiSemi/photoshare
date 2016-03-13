f59e494c695371b7c84fc000be3bcf8bb5566353
unknown <>
2016-03-13 17:11:59 +0900

    Initial commit

---
+++ app/src/main/java/com/github/shawn/cameran/MainActivity.java
@@ -0,0 +1,18 @@
+package com.github.shawn.cameran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String hoge = "hoge";
+        Log.d("Trunk", "onCreate");
+        hoge = "hoga";
+
+    }
+}
